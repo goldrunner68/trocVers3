9387515a415495629bfe8535689793ad171d63f6
goldrunner68 <pilouweb2000@gmail>
2020-04-13 21:25:32 +0200

    FrontEnd trocCELL

--- app/src/main/java/com/example/myapplicationtroc/Article/TrocDisplayActivity.java
+++ app/src/main/java/com/example/myapplicationtroc/Article/TrocDisplayActivity.java
@@ -1,19 +1,17 @@
 package com.example.myapplicationtroc.Article;
 
 import android.database.Cursor;
-import android.util.Log;
-import android.widget.ImageView;
+import android.os.Bundle;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import com.example.myapplicationtroc.R;
 import com.example.myapplicationtroc.bddManager.TrocManager;
-import android.database.Cursor;
+
 import java.util.ArrayList;
 
 public class TrocDisplayActivity extends AppCompatActivity {
-        private ArrayList<Troc> mesTroc;
-        private ListView mMesTrocV;//m(variable)debut et fin V(View)
+    private ArrayList<Troc> mesTroc;
+    private ListView mMesTrocV;//m(variable)debut et fin V(View)
     private TrocAdaptateur trocAdaptateur;
     private TrocManager trocManager;
 
@@ -23,33 +21,38 @@ public class TrocDisplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_troc_display);
 
-        mMesTrocV = (ListView)findViewById(R.id.mesTroc);
-        trocAdaptateur = new TrocAdaptateur(getApplicationContext(),0);
+        mMesTrocV = (ListView) findViewById(R.id.mesTroc);
+        trocAdaptateur = new TrocAdaptateur(getApplicationContext() , 0);
         mesTroc = new ArrayList<>();
 
         TrocManager trocManager = new TrocManager(this); // gestionnaire de la table "troc"
         trocManager.open(); // ouverture de la table en lecture/écriture
         Cursor c = trocManager.getTroc();
-        if (c.moveToFirst())
-        {
+        if (c.moveToFirst()) {
             do {
-
+                // je lis les donnees de ma  BDD comme en PHP
                 String leTitre = c.getString(c.getColumnIndex(TrocManager.KEY_TITRE));
                 String limage = c.getString(c.getColumnIndex(TrocManager.KEY_IMAGE));
                 String leDescriptif = c.getString(c.getColumnIndex(TrocManager.KEY_DESCRIPTIF));
                 String letat = c.getString(c.getColumnIndex(TrocManager.KEY_ETAT));
+
+                // cast mon String limage en int , car un drawable attend un INT
+                // attention ne pas mettre d extention jpg ou autre dans la BDD pour les noms de fichiers
                 int img = getResources().getIdentifier(limage , "drawable" , getPackageName());
                 System.out.println(img);
-                mesTroc.add(new Troc(leTitre ,img, leDescriptif , letat));
+                mesTroc.add(new Troc("Type de produit : "+leTitre , img ,"Descriptif : " +leDescriptif , "Etat du produit : "+letat));
 
             }
             while (c.moveToNext());
         }
-       //mesTroc.add(new Troc("table",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","neuf"));
-       //mesTroc.add(new Troc("chaise",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","occasion"));
-       //mesTroc.add(new Troc("canape",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","occasion"));
-       //mesTroc.add(new Troc("Television",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","neuf"));
-   // j affecte mon addapteur a ma liste
+
+        // version sans BDD en dur
+        //mesTroc.add(new Troc("table",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","neuf"));
+        //mesTroc.add(new Troc("chaise",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","occasion"));
+        //mesTroc.add(new Troc("canape",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","occasion"));
+        //mesTroc.add(new Troc("Television",R.drawable.troclogo,"Lorem Ipsum est tout simplement un texte factice de l'industrie de l'impression et de la composition","neuf"));
+
+        // j affecte mon addapteur a ma liste
         mMesTrocV.setAdapter(trocAdaptateur);
         trocAdaptateur.addAll(mesTroc);
     }
