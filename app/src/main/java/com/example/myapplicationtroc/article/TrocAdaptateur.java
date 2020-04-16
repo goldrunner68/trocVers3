e0068ffe716f3b2264902359aeff8c491646c94a
goldrunner68 <pilouweb2000@gmail>
2020-04-16 10:44:10 +0200

    affichage au clic email

--- app/src/main/java/com/example/myapplicationtroc/article/TrocAdaptateur.java
+++ app/src/main/java/com/example/myapplicationtroc/article/TrocAdaptateur.java
@@ -1,6 +1,8 @@
 package com.example.myapplicationtroc.article;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import com.example.myapplicationtroc.ConnexionActivity;
+import com.example.myapplicationtroc.MainActivity;
 import com.example.myapplicationtroc.R;
 
 public class TrocAdaptateur extends ArrayAdapter<TrocList> {
@@ -27,7 +31,7 @@ public class TrocAdaptateur extends ArrayAdapter<TrocList> {
 
         TextView nomDuTroc = (TextView)view.findViewById(R.id.typeTroc);
         ImageView imageDutroc = (ImageView)view.findViewById(R.id.imageView10);
-        TextView descriptionDuTroc = (TextView)view.findViewById(R.id.trocDescription);
+        final TextView descriptionDuTroc = (TextView)view.findViewById(R.id.trocDescription);
         TextView etatDuTroc = (TextView)view.findViewById(R.id.trocEtat);
         assert currentTrocList != null;
 
@@ -35,6 +39,14 @@ public class TrocAdaptateur extends ArrayAdapter<TrocList> {
         imageDutroc.setImageResource(currentTrocList.getImageArticle());
         descriptionDuTroc.setText(currentTrocList.getDescriptionArticle());
         etatDuTroc.setText(currentTrocList.getEtatDeLarticle());
+        // j ecoute mes Buttons car il attend un evenement
+        descriptionDuTroc.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick( View v ) {// depart de page,  vers l autre page
+                descriptionDuTroc.setText("etudiant@uha.fr");
+            }
+        });
     return  view;
 
     }
