28242434c522c2d2b1d933ae774b03817442c85d
goldrunner68 <pilouweb2000@gmail>
2020-04-18 08:39:51 +0200

    code camera simplifie

--- app/src/main/java/com/example/myapplicationtroc/AddTrocActivity.java
+++ app/src/main/java/com/example/myapplicationtroc/AddTrocActivity.java
@@ -1,37 +1,31 @@
 package com.example.myapplicationtroc;
 
-import android.Manifest;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import com.example.myapplicationtroc.article.TrocDisplayActivity;
 import com.example.myapplicationtroc.bddManager.TrocManager;
 import com.example.myapplicationtroc.bddManager.TrocTable;
 
-import java.io.File;
+
 
 public class AddTrocActivity extends AppCompatActivity {
 
 
-    private static final int REQUEST_ID_READ_WRITE_PERMISSION = 99;
-    private static final int REQUEST_ID_IMAGE_CAPTURE = 100;
-    private static final int REQUEST_ID_VIDEO_CAPTURE = 101;
+
     private static final String TAG = "tag";
+    private static final int CAMERA_REQUEST = 1;
     private VideoView videoView;
     private Button idPrendrePhoto;
     private Button idFileImage;
     private ImageView imageView20;
     private EditText idEditTitreDuTroc, idEditDescription, idEditEtat;
     private Button idAjouterTroc;
+    private AddTrocActivity addTrocActivity;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -45,14 +39,7 @@ public class AddTrocActivity extends AppCompatActivity {
         this.idEditDescription = (EditText) this.findViewById(R.id.idEditDescription);
         this.idEditEtat = (EditText) this.findViewById(R.id.idEditEtat);
         this.idAjouterTroc = (Button) this.findViewById(R.id.idAjouterTroc);
-        // j ecoute mes Buttons car il attend un evenement
-        idPrendrePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick( View v ) {// ouvre la camera si clic sur btn camera
-                captureImage();
 
-            }
-        });
         // j ecoute mes Buttons Ajouter image ,description titre dans la bdd
         idAjouterTroc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +48,26 @@ public class AddTrocActivity extends AppCompatActivity {
 
             }
         });
+
+        // j ecoute mes Buttons car il attend un evenement
+        idPrendrePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {// capture de l image
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
+    });
+
+}// affiche l image l activity actuelle, avec son request code 1
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode , resultCode , data);
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView20.setImageBitmap(photo);
+        }
     }
+
+
 
     private void ajouterToutBdd() {
         TrocManager m = new TrocManager(this); // gestionnaire de la table "animal"
@@ -79,57 +85,9 @@ public class AddTrocActivity extends AppCompatActivity {
         startActivity(new Intent(AddTrocActivity.this , TrocDisplayActivity.class));
     }
 
-    private void captureImage() {
-        // Create an implicit intent, for image capture.
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        // Start camera and wait for the results.
-        this.startActivityForResult(intent , REQUEST_ID_IMAGE_CAPTURE);
-    }
 
 
 
 
-
-
-    @Override
-    protected void onActivityResult( int requestCode , int resultCode , Intent data ) {
-        super.onActivityResult(requestCode , resultCode , data);
-
-        if (requestCode == REQUEST_ID_IMAGE_CAPTURE) {
-            if (resultCode == RESULT_OK) {
-                Bitmap bp = (Bitmap) data.getExtras().get("data");
-                this.imageView20.setImageBitmap(bp);
-
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this , "Action annulée" , Toast.LENGTH_LONG).show();
-
-            } else {
-                Toast.makeText(this , "Petite érreur" , Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-    //getPhotoFileUri : cette fonction, qui prend un paramètre une String correpondant au nom du fichier, va nous retourner un
-    // URI vers l'image prise et stockée la photo sur le disque avec le nom du fichier.
-
-    public Uri getPhotoFile( String fileName ) {
-        if (isExternalStorageAvailable()) {
-            File mediaStorageDir = new File(
-                    getExternalFilesDir(Environment.DIRECTORY_PICTURES) , TAG);
-            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-                Log.d(TAG , "erreur de creation du repertoire");
-            }
-            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
 
         }
-        return null;
-    }
-
-    //isExternalStorageAvailable : cette méthode nous permet de savoir
-    // si un stockage externe pour les photos est disponible
-    private boolean isExternalStorageAvailable() {
-        String state = Environment.getExternalStorageState();
-        return state.equals(Environment.MEDIA_MOUNTED);
-    }
-
-}
